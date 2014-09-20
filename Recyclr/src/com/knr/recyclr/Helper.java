4e34606b00063053f1c2f992873e2a7eff5e79c5
RYAN ORR <rworr@uwaterloo.ca>
2014-09-20 18:18:05 -0400

    Fixing Neils wierd coding style

--- Recyclr/src/com/knr/recyclr/Helper.java
+++ Recyclr/src/com/knr/recyclr/Helper.java
@@ -9,25 +9,16 @@ public class Helper {
 	public static String loadJSONFromAsset(Context context, String assetFileName) {
 	    String json = null;
 	    try {
-
 	        InputStream is = context.getAssets().open(assetFileName);
-
 	        int size = is.available();
-
 	        byte[] buffer = new byte[size];
-
 	        is.read(buffer);
-
 	        is.close();
-
 	        json = new String(buffer, "UTF-8");
-	        System.out.println(json);
-
 	    } catch (IOException ex) {
 	        ex.printStackTrace();
 	        return null;
 	    }
 	    return json;
-
 	}
 }
