15e29647414032c70f4d267b7a1c2f1c1cb2f700
Neil Raina <neil.raina@hotmail.com>
2014-09-20 21:29:59 -0400

    some more recycling information

--- Recyclr/src/com/knr/recyclr/NobarcodeActivity.java
+++ Recyclr/src/com/knr/recyclr/NobarcodeActivity.java
@@ -1,17 +1,65 @@
 package com.knr.recyclr;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class NobarcodeActivity extends ActionBarActivity {
-
+	ListView listView ;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_nobarcode);
 		getActionBar().hide();
+		
+		// Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list_nobarcodeitems);
+        
+        // Defined Array values to show in ListView
+        ArrayList<NoBarcodeItem> values = new ArrayList<NoBarcodeItem>();
+        values.add(new NoBarcodeItem("Paper", NoBarcodeItem.DisposeType.Recycle, null));
+        
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<NoBarcodeItem> adapter = new ArrayAdapter<NoBarcodeItem>(
+        		this, R.layout.recyclr_listview, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter); 
+        
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> parent, View view,
+             int position, long id) {
+            
+           // ListView Clicked item index
+           int itemPosition = position;
+           
+           // ListView Clicked item value
+           NoBarcodeItem  itemValue = (NoBarcodeItem) listView.getItemAtPosition(position);
+              
+            // Show Alert 
+            Toast.makeText(getApplicationContext(),
+              "Position :"+itemPosition+"  ListItem : " +itemValue.toString() , Toast.LENGTH_LONG)
+              .show();
+          }
+        }); 
 	}
 
 	@Override
